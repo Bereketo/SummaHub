e47a631b230b76b264831fbe44acb101d4fb73d5
abfiker <new94abemelek@gmail.com>
2024-06-08 01:41:51 +0300

    user profile

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -1,20 +1,26 @@
 import React, { createContext, useState} from 'react';
-// Import jwtDecode from jwt-decode library
 
 const UserContext = createContext();
 
 export const useUser = () => React.useContext(UserContext);
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-   
+    const [user, setUser] = useState(() => {
+        // Retrieve user data from localStorage when component mounts
+        const storedUser = localStorage.getItem('user');
+        return storedUser ? JSON.parse(storedUser) : null;
+    });
 
     const login = (userData) => {
         setUser(userData);
+        // Store user data in localStorage when logging in
+        localStorage.setItem('user', JSON.stringify(userData));
     };
 
     const logout = () => {
         setUser(null);
+        // Clear user data from localStorage when logging out
+        localStorage.removeItem('user');
     };
 
     return (
