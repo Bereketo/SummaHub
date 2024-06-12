df7df96f804b4c24e67d197a9228f2773a701fe2
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-06-12 06:09:09 +0300

    change some style

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -7,22 +7,33 @@ import Question from "./pages/Question/question";
 import Summary from "./pages/Summary/summary";
 import About from "./pages/HomePage/components/AboutUs/aboutus";
 import { BrowserRouter , Routes, Route, Switch } from "react-router-dom";
+import Admin from './pages/Admin/admin'
+import Chat from "./pages/Chat/chat";
+
+import { useState } from "react";
+import Container from "quill/blots/container";
 
 
 function App() {
+  const  [theme , setTheme] = useState('light')
   return (
-    <div>
+    <div className="container" theme = {theme}>
     <BrowserRouter>
     <Routes>
-       <Route  path = "/" element={<Home/>}/>
+       <Route  path = "/" element={<Home theme = {theme} setTheme = {setTheme}/>}/>
       <Route  path = "/Signup" element={<SignUp/>}/>
-      <Route  path = "/Note" element={<Note/>}/>
-      <Route  path = "/Login" element={<Login />}/>
-      <Route path = '/Question' element = {<Question/>} />
-      <Route path = '/Summary' element = {<Summary/>} />
-      <Route path = '/About' element = {<About/>} />
+      <Route  path = "/Note" element={<Note theme = {theme} setTheme = {setTheme}/>}/>
+      <Route  path = "/Login" element={<Login theme = {theme} setTheme = {setTheme}/>}/>
+      <Route path = '/Question' element = {<Question theme = {theme} setTheme = {setTheme}/>} />
+      <Route path = '/Summary' element = {<Summary theme = {theme} setTheme = {setTheme}/>} />
+      <Route path = '/Chat' element = {<Chat theme = {theme} setTheme = {setTheme}/>} />
+      {/* <Route path = '/About' element = {<About/>} /> */}
+      
        </Routes>
     </BrowserRouter>
+  {/* <TestTypewriter /> */}
+    {/* <Admin /> */}
+
     
     </div>
   );
