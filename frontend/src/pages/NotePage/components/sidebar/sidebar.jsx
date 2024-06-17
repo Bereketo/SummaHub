261af5b9a8ef8dca3613170b8aa6a488604e61f2
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-06-17 17:49:59 +0300

    update sidebar

--- frontend/src/pages/NotePage/components/sidebar/sidebar.jsx
+++ frontend/src/pages/NotePage/components/sidebar/sidebar.jsx
@@ -27,23 +27,23 @@ const Sidebar = ({ setShowNoteArea }) => {
 
     <div className={styles.sidebar_wrapper}>
       <div className={styles.addbtn_wrapper}>
-      <NavLink to="/NoteAdd" className={({ isActive }) => isActive ? styles.active : ''} >
+      <NavLink to="/NoteAdd" className={({ isActive }) => 
+        isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+      } >
       + New Note
       </NavLink>
-    
       </div>
       <div className={styles.sidebar_menu}>
-  
-      <NavLink to="/notefront" className={({ isActive }) => isActive ? styles.active : ''} >
+      <NavLink to="/notefront" className={({ isActive }) =>isActive ? `${styles.navLink_bott} ${styles.active}` : styles.navLink_bott} >
         <strong className={styles.emoji}> 📃</strong> Notes
       </NavLink>
-      <NavLink to="/reminders" className={({ isActive }) => isActive ? styles.active : ''} onClick={() => setShowNoteArea('reminder')}>
+      <NavLink to="/reminders" className={({ isActive }) => isActive ? `${styles.navLink_bott} ${styles.active}` : styles.navLink_bott} onClick={() => setShowNoteArea('reminder')}>
         <strong className={styles.emoji}> 🕒</strong> Reminders
       </NavLink>
-      <NavLink to="/pinned" className={({ isActive }) => isActive ? styles.active : ''} onClick={() => setShowNoteArea('pinnedNotes')}>
+      <NavLink to="/pinned" className={({ isActive }) =>isActive ? `${styles.navLink_bott} ${styles.active}` : styles.navLink_bott} onClick={() => setShowNoteArea('pinnedNotes')}>
         <strong className={styles.emoji}> 🧷</strong> Pinned Notes
       </NavLink>
-      <NavLink to="/trash" className={({ isActive }) => isActive ? styles.active : ''} onClick={() => setShowNoteArea('trashedNotes')}>
+      <NavLink to="/trash" className={({ isActive }) => isActive ? `${styles.navLink_bott} ${styles.active}` : styles.navLink_bott} onClick={() => setShowNoteArea('trashedNotes')}>
         <strong className={styles.emoji}> 🗑️</strong> Trashed
       </NavLink>
       </div>
