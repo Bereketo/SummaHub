980453456dc44094d92f5d361aa823bf07aad46b
abfiker <new94abemelek@gmail.com>
2024-06-18 10:02:00 +0300

    better looking for no trash condition

--- frontend/src/pages/NotePage/components/TrashNote/TrashNote.jsx
+++ frontend/src/pages/NotePage/components/TrashNote/TrashNote.jsx
@@ -13,16 +13,17 @@ function TrashNote() {
     const fetchNotes = async () => {
       try {
         const userToken = JSON.parse(localStorage.getItem('user'));
-        const { token } = userToken;
-        if (!token) {
+        if (!userToken || !userToken.token) {
           throw new Error('No token found');
         }
-        const response = await axios.get(`http://localhost:4040/api/v1/notes/trash?page=${page}`, {
+        const { token } = userToken;
+
+        const response = await axios.get(`http://localhost:4040/api/v1/notes/trashed?page=${page}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-        console.log(response.data)
+
         setNotes(response.data.data);
         setTotalPages(response.data.totalPages);
       } catch (err) {
@@ -38,17 +39,18 @@ function TrashNote() {
   const handleRestore = async (id) => {
     try {
       const userToken = JSON.parse(localStorage.getItem('user'));
-      const { token } = userToken;
-      if (!token) {
+      if (!userToken || !userToken.token) {
         throw new Error('No token found');
       }
+      const { token } = userToken;
+
       await axios.patch(`http://localhost:4040/api/v1/notes/${id}`, { isDeleted: false }, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
+
       setNotes(notes.filter(note => note._id !== id));
-      window.location.reload();
     } catch (err) {
       console.error('Error restoring note:', err);
     }
@@ -62,45 +64,53 @@ function TrashNote() {
 
   return (
     <div>
-      <div className={styles.grid_container}>
-        {notes.map((note) => (
-          <div key={note._id} className={styles.card_wrapper}>
-            <div className={styles.notecontent}>
-              <div className={styles.card_title}>
-                <h1>{note.title}</h1>
+      {notes.length > 0 ? (
+        <div>
+          <div className={styles.grid_container}>
+            {notes.map((note) => (
+              <div key={note._id} className={styles.card_wrapper}>
+                <div className={styles.notecontent}>
+                  <div className={styles.card_title}>
+                    <h1>{note.title}</h1>
+                  </div>
+                  <div className={styles.card_content}>
+                    <p>{note.content}</p>
+                  </div>
+                </div>
+                <footer>
+                  <div className={styles.deleteSave}>
+                    <button
+                      className={styles.restorebtn}
+                      onClick={() => handleRestore(note._id)}
+                    >
+                      Restore
+                    </button>
+                  </div>
+                </footer>
               </div>
-              <div className={styles.card_content}>
-                <p>{note.content}</p>
-              </div>
-            </div>
-            <footer>
-              <div className={styles.deleteSave}>
-                <button
-                  className={styles.restorebtn}
-                  onClick={() => handleRestore(note._id)}
-                >
-                  Restore
-                </button>
-              </div>
-            </footer>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className={styles.pagination}>
-        <button
-          disabled={page === 1}
-          onClick={() => handlePageChange(page - 1)}
-        >
-          Previous
-        </button>
-        <span>Page {page} of {totalPages}</span>
-        <button
-          disabled={notes.length < 6}
-          onClick={() => handlePageChange(page + 1)}
-        >
-          Next
-        </button>
-      </div>
+          <div className={styles.pagination}>
+            <button
+              disabled={page === 1}
+              onClick={() => handlePageChange(page - 1)}
+            >
+              Previous
+            </button>
+            <span>Page {page} of {totalPages}</span>
+            <button
+              disabled={page === totalPages}
+              onClick={() => handlePageChange(page + 1)}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p style={{ width: '100%', height: '100%', backgroundColor: 'lightblue', borderRadius: '12px', border: '1px solid white', padding: '20px', textAlign: 'center' }}>
+          No trash notes
+        </p>
+      )}
     </div>
   );
 }
