9a9ab6d756f2ddd6d1158cf75fe1282b4de8b22b
Berekto <bereketalebachew87@gmail.com>
2024-06-19 09:41:10 +0300

    add trashed users

--- web/backend/routes/userRoutes.js
+++ web/backend/routes/userRoutes.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const UserController = require('../controllers/userController')
+const UserController = require('../controllers/userController');
 const AuthController = require('../controllers/authController');
 
+const router = express.Router();
 
+router.post('/signup', AuthController.signup);
+router.post('/login', AuthController.login);
+router.get('/logout', AuthController.logout);
+router.post('/forgotPassword', AuthController.forgotPassword);
+router.patch('/resetPassword/:token', AuthController.resetPassword);
 
-const router = express.Router()
-router.post('/signup', AuthController.signup)
-router.post('/login', AuthController.login)
-router.get('/logout', AuthController.logout)
-router.post('/forgotPassword', AuthController.forgotPassword)
-router.patch('/resetPassword/:token', AuthController.resetPassword)
+router.use(AuthController.protect);
 
-router.use(AuthController.protect)
+router.patch('/updatePassword/:id', AuthController.updatePassword);
+router.get('/me', UserController.getMe, UserController.getUser);
+router.patch('/updateMe', UserController.uploadUserPhoto, UserController.resizeUserPhoto, UserController.updateMe);
+router.delete('/deleteMe', UserController.deleteMe);
 
-router.patch('/updatePassword/:id', AuthController.updatePassword)
-router.get('/me', UserController.getMe, UserController.getUser)
-router.patch('/updateMe', UserController.uploadUserPhoto,UserController.resizeUserPhoto, UserController.updateMe)
-router.delete('/deleteMe', UserController.deleteMe)
-
-router.use(AuthController.restrictTo('admin'))
-router.route('/').get(UserController.getAllusers)
+router.use(AuthController.restrictTo('admin'));
+router.route('/trashedUsers').
+    get(UserController.trashedUser);  
+router.route('/')
+    .get(UserController.getAllusers);
 router.route('/:id')
     .patch(UserController.deleteUser)
     .patch(UserController.updateUser)
-    .get(UserController.getUser)
-module.exports = router
+    .get(UserController.getUser);
+
+module.exports = router;
