66b6068fdaff8e153264f2145819a62f174f5069
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-06-19 14:47:46 +0300

    Reminder Update

--- frontend/src/pages/NotePage/components/Reminder/reminder.jsx
+++ frontend/src/pages/NotePage/components/Reminder/reminder.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import styles from "./reminder.module.css";
-import Header from "../../../HomePage/components/header/header";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Header from '../../../HomePage/components/header/header'
 import Sidebar from "../sidebar/sidebar";
-import axios from "axios";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { format } from 'date-fns';
 
-function Reminder({ theme, setTheme }) {
+function Reminder() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes().toString().padStart(2, "0");
@@ -44,8 +42,6 @@ function Reminder({ theme, setTheme }) {
   const [remDate, setRemDate] = useState("");
   const [remTime, setRemTime] = useState("");
   const [reminders, setReminders] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(3);
 
   useEffect(() => {
     if (Notification.permission !== "granted") {
@@ -54,32 +50,15 @@ function Reminder({ theme, setTheme }) {
     fetchReminders();
   }, []);
 
-  const fetchReminders = async () => {
-    try {
-      const userToken = JSON.parse(localStorage.getItem('user'));
-      if (!userToken || !userToken.token) {
-        throw new Error('No token found');
-      }
-      const { token } = userToken;
-      const response = await axios.get("http://localhost:4040/api/v1/reminders", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      // Convert datetime strings to Date objects for frontend processin
-      const formattedReminders = response.data.data.map(reminder => ({
-        ...reminder,
-        datetime: new Date(reminder.datetime)
-      }));
-      setReminders(formattedReminders);
-
-
-      setReminders(formattedReminders);
-    } catch (error) {
-      console.error("Error fetching reminders", error);
-
+  const fetchReminders = () => {
+    const storedReminders = localStorage.getItem("reminders");
+    if (storedReminders) {
+      setReminders(JSON.parse(storedReminders));
     }
+  };
+
+  const saveReminders = (reminders) => {
+    localStorage.setItem("reminders", JSON.stringify(reminders));
   };
 
   const handleTitleChange = (event) => {
@@ -98,82 +77,48 @@ function Reminder({ theme, setTheme }) {
     setRemTime(event.target.value);
   };
 
-  const handleSave = async () => {
+  const handleSave = () => {
     if (!remTitle || !remDescription || !remDate || !remTime) {
       toast.error("Please fill in all fields.");
       return;
     }
-
-    const reminderDateTime = new Date(`${remDate}T${remTime}`);
-    if (reminderDateTime <= new Date()) {
-      toast.error("Selected time is in the past or current. Please choose a future time.");
-      return;
-    }
-
     const newReminder = {
       title: remTitle,
       description: remDescription,
       date: remDate,
-      time: remTime
+      time: remTime,
     };
-
-    try {
-      const userToken = JSON.parse(localStorage.getItem('user'));
-      if (!userToken || !userToken.token) {
-        throw new Error('No token found');
-      }
-      const { token } = userToken;
-      const response = await axios.post("http://localhost:4040/api/v1/reminders", newReminder, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setReminders([...reminders, response.data.data]);
-      scheduleNotification(response.data.data);
-      setRemTitle("");
-      setRemDescription("");
-      setRemDate("");
-      setRemTime("");
-      toast.success("Reminder saved successfully!");
-      console.log(response.data.data);
-    } catch (error) {
-      console.error("Error saving reminder", error);
-      toast.error("Error saving reminder");
-    }
+    const updatedReminders = [...reminders, newReminder];
+    setReminders(updatedReminders);
+    saveReminders(updatedReminders);
+    scheduleNotification(newReminder);
+    setRemTitle("");
+    setRemDescription("");
+    setRemDate("");
+    setRemTime("");
+    toast.success("Reminder saved successfully!");
   };
-
 
   const scheduleNotification = (reminder) => {
-    const reminderDateTime = new Date(reminder.time);
+    const reminderDateTime = new Date(`${reminder.date}T${reminder.time}`);
     const timeDifference = reminderDateTime.getTime() - new Date().getTime();
-    console.log(reminderDateTime,timeDifference)
-    if (timeDifference < 0) {
+  
+    if (timeDifference > 0) {
       setTimeout(() => {
         sendNotification(reminder.title, reminder.description);
-        removeReminder(reminder._id); // Adjusted to use reminder ID
+        removeReminder(reminder); // Remove the reminder after notification is shown
       }, timeDifference);
+    } else {
+      toast.error("Selected time is in the past. Please choose a future time.");
     }
   };
-
-  const removeReminder = async (id) => {
-    try {
-      const userToken = JSON.parse(localStorage.getItem('user'));
-      if (!userToken || !userToken.token) {
-        throw new Error('No token found');
-      }
-      const { token } = userToken;
-      await axios.delete(`http://localhost:4040/api/v1/reminders/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setReminders(reminders.filter((r) => r._id !== id));
-      toast.success("Reminder deleted successfully!");
-    } catch (error) {
-      console.error("Error deleting reminder", error);
-      toast.error("Error deleting reminder");
-    }
+  
+  const removeReminder = (reminder) => {
+    const updatedReminders = reminders.filter(r => r !== reminder);
+    setReminders(updatedReminders);
+    saveReminders(updatedReminders);
   };
+  
 
   const sendNotification = (title, body) => {
     console.log("Sending notification...");
@@ -187,149 +132,102 @@ function Reminder({ theme, setTheme }) {
       });
     }
   };
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = reminders.slice(indexOfFirstItem, indexOfLastItem);
-
-  const nextPage = () => {
-    if (currentPage < Math.ceil(reminders.length / itemsPerPage)) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const prevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
+  
   return (
     <div className={styles.reminder_wrapper}>
-      <Header useButtons={true} theme={theme} setTheme={setTheme} />
-      <div className={styles.bottom_container}>
-        <Sidebar />
-        <div className={styles.content}>
-          <div className={styles.top_part}>
-            <div className={styles.today_box}>
-              <h1>Today's Date</h1>
-              <h2 className={styles.hour}>
-                {hours}:{minutes}
-              </h2>
-              <h2 className={styles.date}>
-                {`${day}, ${date} ${month} ${year}`}
-              </h2>
-            </div>
-            <div className={styles.reminder_input}>
-              <div className={styles.text_input}>
-                <input
-                  type="text"
-                  placeholder="Title"
-                  className={styles.title_input}
-                  value={remTitle}
-                  onChange={handleTitleChange}
-                />
-                <textarea
-                  type="text"
-                  placeholder="Description"
-                  className={styles.description_input}
-                  value={remDescription}
-                  onChange={handleDescriptionChange}
-                />
-                <button className={styles.remsave} onClick={handleSave}>
-                  Save
-                </button>
-              </div>
-              <div className={styles.date_timeinput}>
-                <div className={styles.date_input}>
-                  <p>Date</p>
-                  <input
-                    type="date"
-                    id="date"
-                    value={remDate}
-                    onChange={handleDateChange}
-                  />
-                </div>
-                <div className={styles.time_input}>
-                  <p>Time</p>
-                  <input
-                    type="time"
-                    id="time"
-                    value={remTime}
-                    onChange={handleTimeChange}
-                  />
-                </div>
-              </div>
-            </div>
+    <Header />
+    <div className={styles.reminder_container}>
+    <Sidebar />
+    <div className={styles.content}>
+      <div className={styles.top_part}>
+        <div className={styles.today_box}>
+          <h1>Today's Date</h1>
+          <h2 className={styles.hour}>
+            {hours}:{minutes}
+          </h2>
+          <h2 className={styles.date}>
+            {day}, {date}th {month}, {year}
+          </h2>
+        </div>
+        <div className={styles.reminder_input}>
+          <div className={styles.text_input}>
+            <input
+              type="text"
+              placeholder="Title"
+              className={styles.title_input}
+              value={remTitle}
+              onChange={handleTitleChange}
+            />
+            <textarea
+              type="text"
+              placeholder="Description"
+              className={styles.description_input}
+              value={remDescription}
+              onChange={handleDescriptionChange}
+            />
+            <button className={styles.remsave} onClick={handleSave}>
+              Save
+            </button>
           </div>
-          <div className={styles.bottom_part}>
-            <table>
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Description</th>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentItems.length === 0 ? (
-                  <tr>
-                    <td colSpan="5">No reminders</td>
-                  </tr>
-                ) : (
-                  currentItems.map((reminder) => (
-                    <tr key={reminder._id}>
-                      <td>{reminder.title}</td>
-                      <td>{reminder.description}</td>
-                      <td>{format(new Date(reminder.date), 'dd MMMM yyyy')}</td>
-                      <td>{reminder.time}</td>
-                      <td>
-                        <button onClick={() => removeReminder(reminder._id)}>
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
-            <div className={styles.pagination_controls}>
-              <button onClick={prevPage} disabled={currentPage === 1}>
-                Previous
-              </button>
-              <button
-                onClick={nextPage}
-                disabled={
-                  currentPage === Math.ceil(reminders.length / itemsPerPage)
-                }
-              >
-                Next
-              </button>
+          <div className={styles.date_timeinput}>
+            <div className={styles.date_input}>
+              <p>Date</p>
+              <input
+                type="date"
+                id="date"
+                value={remDate}
+                onChange={handleDateChange}
+              />
             </div>
-            {reminders.length > itemsPerPage && (
-              <ul className={styles.pagination}>
-                {Array(Math.ceil(reminders.length / itemsPerPage))
-                  .fill()
-                  .map((_, index) => (
-                    <li
-                      key={index}
-                      onClick={() => paginate(index + 1)}
-                      className={currentPage === index + 1 ? styles.active : ""}
-                    >
-                      {index + 1}
-                    </li>
-                  ))}
-              </ul>
-            )}
+            <div className={styles.time_input}>
+              <p>Time</p>
+              <input
+                type="time"
+                id="time"
+                value={remTime}
+                onChange={handleTimeChange}
+              />
+            </div>
           </div>
         </div>
       </div>
-
+      <div className={styles.bottom_part}>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reminders.length === 0 ? (
+              <tr>
+                <td colSpan="5">No reminders</td>
+              </tr>
+            ) : (
+              reminders.map((reminder, index) => (
+                <tr key={index}>
+                  <td>{reminder.title}</td>
+                  <td>{reminder.description}</td>
+                  <td>{reminder.date}</td>
+                  <td>{reminder.time}</td>
+                  <td>
+                    <button onClick={() => removeReminder(reminder)}>Delete</button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+      </div>
+      <ToastContainer />
     </div>
+    </div>
+
   );
 }
 
